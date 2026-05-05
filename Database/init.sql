5e477bbec269b05aa7aa5bd1cbecca247f93c7fc
Thanusha <thanushasb4@gmail.com>
2026-05-06 00:59:03 +0530

    initial project structure

---
+++ Database/init.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE roles (
+    role_id SERIAL PRIMARY KEY,
+    role_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+INSERT INTO roles (role_name) VALUES ('Admin'), ('Sales_Agent'), ('Manager');
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role_id INTEGER REFERENCES roles(role_id),
+    status VARCHAR(20) DEFAULT 'active',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
