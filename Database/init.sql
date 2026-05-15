86c60bf4e069232bafbb25ecb6beb368f9aa560c
Thanusha <thanushasb4@gmail.com>
2026-05-16 02:59:22 +0530

    added subfolder

--- Database/init.sql
+++ Database/init.sql
@@ -1,17 +1,22 @@
-
+-- 1. Create Roles Table
 CREATE TABLE roles (
     role_id SERIAL PRIMARY KEY,
     role_name VARCHAR(50) UNIQUE NOT NULL
 );
 
-INSERT INTO roles (role_name) VALUES ('Admin'), ('Sales_Agent'), ('Manager');
+
+INSERT INTO roles (role_name) VALUES 
+('Admin'), 
+('Sales_Agent'), 
+('Manager'), 
+('Customer');
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    role_id INTEGER REFERENCES roles(role_id),
+    role_id INTEGER REFERENCES roles(role_id) ON DELETE RESTRICT,
     status VARCHAR(20) DEFAULT 'active',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
